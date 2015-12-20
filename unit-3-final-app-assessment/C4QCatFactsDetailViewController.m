fa8edd3efdde4e062842f3c0550768a37fd01924
shenayoshida <sheyoshida@gmail.com>
2015-12-20 13:52:04 -0500

    random cat images populating detail view

--- unit-3-final-app-assessment/C4QCatFactsDetailViewController.m
+++ unit-3-final-app-assessment/C4QCatFactsDetailViewController.m
@@ -7,12 +7,14 @@
 //
 
 #import "C4QCatFactsDetailViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
 
 #define CAT_GIF_URL @"http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
 
 @interface C4QCatFactsDetailViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *factLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -26,6 +28,11 @@
     self.factLabel.text = excitedFact;
     
     [self makeGiphyAPICall];
+    
+    self.imageView.clipsToBounds = YES;
+
+   
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,7 +41,33 @@
 
 - (void)makeGiphyAPICall {
     
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] init];
     
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+ [manager GET:CAT_GIF_URL parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+     
+     // sterialization of the dictionary data so that it isn't a bunch of numbers:
+     NSDictionary *catImages;
+     NSError *error = nil;
+     if (responseObject != nil) {
+         catImages = [NSJSONSerialization JSONObjectWithData:responseObject
+                                                    options:NSJSONReadingMutableContainers
+                                                      error:&error];
+     
+         NSArray *data = [catImages objectForKey:@"data"];
+         NSDictionary *object = [data objectAtIndex:(arc4random_uniform(data.count - 1) + 1)];
+         NSString *imageURL = [[[object objectForKey:@"images"]objectForKey:@"original_still"]objectForKey:@"url"];
+         
+         NSData * catData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: imageURL]];
+         self.imageView.image = [UIImage imageWithData:catData];
+     }
+     
+     [self.view reloadInputViews]; // reload view w API data
+
+ } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+     NSLog(@"ERROR MESSAGE: %@", error.userInfo);
+ }];
     
 }
 
